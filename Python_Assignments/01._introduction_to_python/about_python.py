6da4f16aed00ae1549ba36491a697c049d3b2ade
Rudra Dholakiya <rudra.dholakiya.cg@gmail.com>
2026-08-30 23:54:56 +0530

    Add files via upload

---
+++ Python_Assignments/01._introduction_to_python/about_python.py
@@ -0,0 +1,4 @@
+# Python is a high-level, easy-to-learn programming language.
+
+print("Python is a simple and beginner-friendly programming language.")
+print("I am learning Python and enjoying programming!")
---
+++ Python_Assignments/01._introduction_to_python/welcome.py
@@ -0,0 +1,4 @@
+# Display a welcome message to the user.
+
+print("Welcome to Python Programming!")
+print("Python is a simple and beginner-friendly programming language.")
